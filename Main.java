2d53c8230d613a27fa00d497f90289551cc691e0
Artem Pikalov <aepikalov@gmail.com>
2021-08-14 22:37:35 +0400

    JavaCore_3_1_Files v2

--- Main.java
+++ Main.java
@@ -1,43 +1,47 @@
 package JavaCore_3_1_Files;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-        File games = new File("D://Games");
-        File temp = new File("D://Games//temp");
-        File src = new File("D://Games//src");
-        File res = new File("D://Games//res");
-        File savegames = new File("D://Games//savegames");
-        File main = new File("D://Games//src//main");
-        File test = new File("D://Games//src//test");
-        File mainJava = new File("D://Games//src//main//Main.java");
-        File utilsJava = new File("D://Games//src//main//Utils.java");
-        File drawables = new File("D://Games//res//drawables");
-        File vectors = new File("D://Games//res//vectors");
-        File icons = new File("D://Games//res//icons");
-        File tempTxt = new File("D://Games//temp//temp.txt");
-        try {
-            if (games.mkdir()) sb.append("Каталог \"D:/Games\" создан\n");
-            if (temp.mkdir()) sb.append("Каталог \"D:/Games/temp\" создан\n");
-            if (src.mkdir()) sb.append("Каталог \"D:/Games/src\" создан\n");
-            if (res.mkdir()) sb.append("Каталог \"D:/Games/res\" создан\n");
-            if (savegames.mkdir()) sb.append("Каталог \"D:/Games/savegames\" создан\n");
-            if (main.mkdir()) sb.append("Каталог \"D:/Games/src/main\" создан\n");
-            if (test.mkdir()) sb.append("Каталог \"D:/Games/src/test\" создан\n");
-            if (mainJava.createNewFile()) sb.append("Файл \"D:/Games/src/main/Main.java\" создан\n");
-            if (utilsJava.createNewFile()) sb.append("Файл \"D:/Games/src/main/Utils.java\" создан\n");
-            if (drawables.mkdir()) sb.append("Каталог \"D:/Games/res/drawables\" создан\n");
-            if (vectors.mkdir()) sb.append("Каталог \"D:/Games/res/vectors\" создан\n");
-            if (icons.mkdir()) sb.append("Каталог \"D:/Games/res/icons\" создан\n");
-            if (tempTxt.createNewFile()) sb.append("Файл \"D:/Games/temp/temp.txt\" создан\n");
-            FileWriter log = new FileWriter("D://Games//temp//temp.txt", false);
+        List<File> folderList = Arrays.asList(
+                new File("D://Games"),
+                new File("D://Games//temp"),
+                new File("D://Games//src"),
+                new File("D://Games//res"),
+                new File("D://Games//savegames"),
+                new File("D://Games//src//main"),
+                new File("D://Games//src//test"),
+                new File("D://Games//res//drawables"),
+                new File("D://Games//res//vectors"),
+                new File("D://Games//res//icons")
+        );
+        List<File> fileList = Arrays.asList(
+                new File("D://Games//src//main//Main.java"),
+                new File("D://Games//src//main//Utils.java"),
+                new File("D://Games//temp//temp.txt")
+        );
+        folderList.stream().forEach(folder -> {
+            if (folder.mkdir()) sb.append("Каталог " + folder + " создан\n");
+            else sb.append("Каталог " + folder + " не создан\n");
+        });
+        fileList.stream().forEach(file -> {
+            try {
+                if (file.createNewFile()) sb.append("Файл " + file + " создан\n");
+                else sb.append("Файл " + file + " не создан\n");
+            } catch (IOException ex) {
+                sb.append(ex.getMessage() + '\n');
+            }
+        });
+        try (FileWriter log = new FileWriter("D://Games//temp//temp.txt", false)) {
             log.write(sb.toString());
             log.flush();
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            sb.append(ex.getMessage() + '\n');
         }
         try (BufferedReader br = new BufferedReader(new FileReader("D://Games//temp//temp.txt"))) {
             String s;
